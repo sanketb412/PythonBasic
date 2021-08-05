89170e920ee0f1fca7a6ef9db01530823353406f
sanketb412 <sanketbagde38@gmail.com>
2021-08-05 15:28:32 +0530

    Add: code for getting input from user

---
+++ Basic Core/input.py
@@ -0,0 +1,10 @@
+# ''""
+# @Author: Sanket Bagde
+# @Date: 2021-05-08
+# @Last Modified by:
+# @Last Modified time:Saket
+# @Title : taken user input and display it
+# '''
+
+username = input("Enter User Name: ")
+print("Hello " + username + ", How are you?")
