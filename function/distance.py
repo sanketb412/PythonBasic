b69c0228fd7dcd5db5866d3c7fb20850c5c21a51
sanketb412 <sanketbagde38@gmail.com>
2021-08-06 17:26:48 +0530

    [Sanket B] Add: code for finding distance between point and origin

---
+++ function/distance.py
@@ -0,0 +1,17 @@
+# ''""
+# @Author: Sanket Bagde
+# @Date: 2021-05-08
+# @Last Modified by: 
+# @Last Modified time: 
+# @Title : Print Distance between two points provided by User.
+# '''
+
+import math
+
+def point_distance(i, j):
+    return math.sqrt((i*i) + (j*j))
+
+x = int(input("Enter x-cordinate:- "))
+y = int(input("Enter y-cordinate:- "))
+
+print("Euclidean distance is:-",point_distance(x, y))
