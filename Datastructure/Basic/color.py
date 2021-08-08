5187020337a15d0cfabd01d27ccf92b4585c01b0
sanketb412 <sanketbagde38@gmail.com>
2021-08-08 23:15:14 +0530

    [Sanket B] Add: code for display first and last element from list

---
+++ Datastructure/Basic/color.py
@@ -0,0 +1,10 @@
+# ''""
+# @Author: Sanket Bagde
+# @Date: 2021-05-08
+# @Last Modified by: Sanket
+# @Last Modified time: 12:30pm
+# @Title : to display the first and last colors from the following list.
+# '''
+
+color_list=["Red","Green","White","Black"]
+print(color_list[0], color_list[-1])
