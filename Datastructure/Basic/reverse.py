bb5fec4199425b130bbf3163a635dba274771e31
sanketb412 <sanketbagde38@gmail.com>
2021-08-08 22:25:34 +0530

    [Sanket B] Add: code for reversing user Input Name

---
+++ Datastructure/Basic/reverse.py
@@ -0,0 +1,9 @@
+# ''""
+# @Author: Sanket Bagde
+# @Date: 2021-05-08
+# @Last Modified by: Sanket
+# @Last Modified time: 12:30pm
+# @Title : Print user Input in Reverse
+# '''
+
+print(input("Enter First and last name:- ")[::-1])
--- function/quadraticRoot.py
+++ function/quadraticRoot.py
@@ -8,13 +8,22 @@
 
 import math
 
-a = int(input("Enter co-efficient for x^2:- "))
-b = int(input("Enter co-efficient for x:- "))
-c = int(input("Enter constant co-efficient:- "))
+def get_input():
+    a = int(input("Enter co-efficient for x^2:- "))
+    b = int(input("Enter co-efficient for x:- "))
+    c = int(input("Enter constant co-efficient:- "))
+    return a, b, c
 
-delta = b*b - 4*a*c
+def root(a, b, c):
 
-root1_x = (-b + math.sqrt(delta))/(2*a)
-root2_x = (-b - math.sqrt(delta))/(2*a)
+    delta = b*b - 4*a*c
 
-print("The roots of qudratic equation is:- ", root1_x, root2_x)
+    root1_x = (-b + math.sqrt(delta))/(2*a)
+    root2_x = (-b - math.sqrt(delta))/(2*a)
+
+    print("The roots of qudratic equation is:- ", root1_x, root2_x)
+
+if __name__ == '__main__':
+    a, b, c = get_input()
+    root(a, b, c)
+    
