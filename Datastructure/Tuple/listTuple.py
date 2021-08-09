d62f1cf6d8a2ecf4e687bf7a582e81f0f6226b2d
sanketb412 <sanketbagde38@gmail.com>
2021-08-09 15:48:46 +0530

    [Sanket B] Add: code to convert list into tuple

---
+++ Datastructure/Tuple/listTuple.py
@@ -0,0 +1,11 @@
+# ''""
+# @Author: Sanket Bagde
+# @Date: 2021-09-08
+# @Last Modified by:
+# @Last Modified time:
+# @Title : Write a Python program to convert a list to a tuple.
+# '''
+
+mylist = [2,4,5,67,7,4]
+mytuple = tuple(mylist)
+print(mytuple)
