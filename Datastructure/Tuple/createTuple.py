82600bda38544c3afbfda53532e6b589a8de8407
sanketb412 <sanketbagde38@gmail.com>
2021-08-09 15:26:53 +0530

    [Sanket B] Add: code to create a tuple

---
+++ Datastructure/Tuple/createTuple.py
@@ -0,0 +1,10 @@
+# ''""
+# @Author: Sanket Bagde
+# @Date: 2021-09-08
+# @Last Modified by:
+# @Last Modified time:
+# @Title :Write a Python program to create a tuple.
+# '''
+
+mytuple = ("tuple", 3, False, 1)
+print(mytuple)
